d581a3871d10ee18ef9b9e65501e1cafe094c756
sb-synth <stefano.bosisio@synthesia.io>
2024-02-16 21:14:57 +0000

    add region

--- 2_window_features/scripts/run_flex_template.sh
+++ 2_window_features/scripts/run_flex_template.sh
@@ -2,6 +2,7 @@
 
 PIPELINE_NAME="window-pipeline"
 PROJECT="long-axle-412512"
+REGION="us-central2"
 DATAFLOW_GCS_LOCATION="gs://flex_templates_my_pipeline/window_template.json"
 NUM_MAX_WORKERS=2
 
@@ -9,8 +10,8 @@ echo "Running Flex Template"
 gcloud dataflow flex-template run ${PIPELINE_NAME} \
 --project=${PROJECT} \
 --template-file-gcs-location=${DATAFLOW_GCS_LOCATION} \
---worker-region=us-central1 \
---region=us-central1 \
+--worker-region=${REGION} \
+--region=${REGION} \
 --worker-machine-type=n1-standard-2 \
 --max-workers=$NUM_MAX_WORKERS  \
 --num-workers=1  \
@@ -18,6 +19,6 @@ gcloud dataflow flex-template run ${PIPELINE_NAME} \
 --staging-location=gs://dataflow-staging-europe-west2-1028464732444/ \
 --parameters job_name=window-pipeline \
 --parameters project=${PROJECT} \
---parameters region=us-central1 \
+--parameters region=${REGION} \
 --parameters input-file=gs://input_files_my_pipeline/input_file.txt \
 --parameters output-file=gs://input_files_my_pipeline/output
