f353a5d63384bfaebf6b5d8fca19aa0643f21cfd
sb-synth <stefano.bosisio@synthesia.io>
2024-02-19 09:35:21 +0000

    test on pubsub

--- 2_window_features/generate_data.py
+++ 2_window_features/generate_data.py
@@ -2,7 +2,10 @@ from google.cloud import pubsub_v1
 import time
 import random
 import json
+from structlog import get_logger
 
+
+logger = get_logger()
 project_id = "long-axle-412512"
 topic_id = "example-window-pipeline"
 
@@ -15,6 +18,7 @@ def generate_data():
             "timestamp": int(time.time()),
             "value": random.randint(1, 100)
         }
+        logger.info(f"Publishing {data} to {topic_path}")
         message = json.dumps(data)
         publisher.publish(topic_path, message.encode("utf-8"))
         time.sleep(1)
