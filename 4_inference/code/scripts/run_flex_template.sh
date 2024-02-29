1383381c5e8a98ed77ff0f634c79946702a4bfe7
sb-synth <stefano.bosisio@synthesia.io>
2024-02-29 23:21:48 +0000

    fix

--- 4_inference/code/scripts/run_flex_template.sh
+++ 4_inference/code/scripts/run_flex_template.sh
@@ -17,10 +17,9 @@ gcloud dataflow flex-template run ${PIPELINE_NAME} \
 --num-workers=5  \
 --temp-location=gs://mypipelines-dataflow-temp/ \
 --staging-location=gs://dataflow-staging-europe-west2-1028464732444/ \
---worker_harness_container_image="europe-west2-docker.pkg.dev/long-axle-412512/whisper-pipeline/whisper_pipeline_flex:latest" \
 --parameters job_name=window-pipeline \
 --parameters project=${PROJECT} \
 --parameters region=${REGION} \
 --parameters disk_size_gb=50 \
---parmaeters dataflow_service_options="worker_accelerator=type:nvidia-tesla-t4;count:1;install-nvidia-driver" \
---parameters dataflow_service_options="worker_harness_container_image=europe-west2-docker.pkg.dev/long-axle-412512/whisper-pipeline/whisper_pipeline_flex:latest"
+--parameters dataflow_service_options="worker_accelerator=type:nvidia-tesla-t4;count:1;install-nvidia-driver" \
+--parameters worker_harness_container_image="europe-west2-docker.pkg.dev/long-axle-412512/whisper-pipeline/whisper_pipeline_flex:latest"
