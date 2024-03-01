dd1701aa9462cef938b40daecbef0354d71c2e36
sb-synth <stefano.bosisio@synthesia.io>
2024-03-01 09:29:13 +0000

    fix builds

--- 4_inference/code/scripts/build_flex_template.sh
+++ 4_inference/code/scripts/build_flex_template.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-echo "Build Docker image"
-docker build --no-cache -t whisper_pipeline -f docker/worker.Dockerfile .
-echo "Tag Docker image"
-docker tag whisper_pipeline europe-west2-docker.pkg.dev/long-axle-412512/whisper-pipeline/whisper_pipeline:latest
-echo "Push Docker image"
-docker push europe-west2-docker.pkg.dev/long-axle-412512/whisper-pipeline/whisper_pipeline:latest
-
-echo "Build the Flex Image"
-docker build --no-cache -t whisper_pipeline_flex -f docker/flex.Dockerfile .
-echo "Tag Docker image"
-docker tag whisper_pipeline_flex europe-west2-docker.pkg.dev/long-axle-412512/whisper-pipeline/whisper_pipeline_flex:latest
-echo "Push Docker image"
-docker push europe-west2-docker.pkg.dev/long-axle-412512/whisper-pipeline/whisper_pipeline_flex:latest
-
-
 echo "*************************"
 echo "Build Flex Template"
 gcloud dataflow flex-template build gs://flex_templates_my_pipeline/whisper_template.json \
