48add80234bfb6eda5a8509ad10332d8e1eb0747
sb-synth <stefano.bosisio@synthesia.io>
2024-03-05 10:22:53 +0000

    update repos

--- 3_sawtooh_window/scripts/build_flex_template.sh
+++ 3_sawtooh_window/scripts/build_flex_template.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 echo "Build Docker image"
-docker build --no-cache -t window_pipeline -f docker/Dockerfile .
+docker build --no-cache -t sawtooh_window_pipeline -f docker/Dockerfile .
 echo "Tag Docker image"
-docker tag window_pipeline europe-west2-docker.pkg.dev/long-axle-412512/window-pipeline/window_pipeline:latest
+docker tag sawtooh_window_pipeline europe-west2-docker.pkg.dev/long-axle-412512/sawtooth-window-pipeline/sawtooh_window_pipeline:latest
 echo "Push Docker image"
-docker push europe-west2-docker.pkg.dev/long-axle-412512/window-pipeline/window_pipeline:latest
+docker push europe-west2-docker.pkg.dev/long-axle-412512/sawtooth-window-pipeline/sawtooh_window_pipeline:latest
 
 echo "*************************"
 echo "Build Flex Template"
-gcloud dataflow flex-template build gs://flex_templates_my_pipeline/window_template.json \
---image-gcr-path europe-west2-docker.pkg.dev/long-axle-412512/window-pipeline/window_pipeline:latest \
+gcloud dataflow flex-template build gs://flex_templates_my_pipeline/sawtooh_window_template.json \
+--image-gcr-path europe-west2-docker.pkg.dev/long-axle-412512/sawtooth-window-pipeline/sawtooh_window_pipeline:latest \
 --sdk-language "PYTHON" \
 --metadata-file metadata/metadata.json \
 --project long-axle-412512 \
