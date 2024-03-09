53b7d97609132f2adde3f3d01f3dc3a6c5667d55
sb-synth <stefano.bosisio@synthesia.io>
2024-03-09 09:16:54 +0000

    check

--- 2_window_features/scripts/run_dataflow_runner.sh
+++ 2_window_features/scripts/run_dataflow_runner.sh
@@ -8,8 +8,8 @@ NUM_MAX_WORKERS=2
 echo "Running pipeline"
 
 python pipeline/processing_logs.py \
-    --input_subscription projects/long-axle-412512/subscriptions/example-window-pipeline-sub \
-    --output_topic projects/long-axle-412512/topics/example-output-window-pipeline \
+    --input-subscription projects/long-axle-412512/subscriptions/example-window-pipeline-sub \
+    --output-topic projects/long-axle-412512/topics/example-output-window-pipeline \
     --runner DataflowRunner \
     --region ${REGION} \
     --project ${PROJECT} \
