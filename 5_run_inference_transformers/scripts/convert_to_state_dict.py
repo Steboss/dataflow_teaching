061b52fad7c777a107b596a1ef38367f1cfcd6fb
Steboss <Steboss>
2024-03-11 14:59:13 +0000

    update gptj

--- 5_run_inference_transformers/scripts/convert_to_state_dict.py
+++ 5_run_inference_transformers/scripts/convert_to_state_dict.py
@@ -1,7 +1,7 @@
 import torch
 from transformers import T5ForConditionalGeneration
 
-model = T5ForConditionalGeneration.from_pretrained("/home/sbosisio486/dataflow_teaching/from_tutorial/t5_model", local_files_only=True)
+model = T5ForConditionalGeneration.from_pretrained("/home/sbosisio486/dataflow_teaching/5_run_inference_transformers/gptj_model", local_files_only=True)
 print("passing model to cpu") 
 model.to('cpu')
 print("saving dictionary")
--- 5_run_inference_transformers/scripts/run_dataflow_runner.sh
+++ 5_run_inference_transformers/scripts/run_dataflow_runner.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
 
 python3 pipeline/main.py --runner DataflowRunner \
-   --model_state_dict_path gs://ggml_models/state_dict.pth \
-   --model_name t5-small \
+   --model_state_dict_path gs://ggml_models/gptj.pth \
+   --model_name "EleutherAI/gpt-j-6B" \
    --project long-axle-412512 \
    --region us-central1 \
    --requirements_file requirements.txt \
